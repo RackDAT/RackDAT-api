aa7e1891aac518b60a5f6bd7321c68aa90008460
MrJ8585 <josea.solis@cetys.edu.mx>
2023-04-11 08:58:39 -0700

    watafac estoy haciendo

---
+++ Contracts/CarreraResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RackDAT_API.Contracts;
+
+public class CarreraResponse
+{
+    public int ID { get; set; }
+    public string carrera { get; set; }
+
+}
---
+++ Models/Carrera.cs
@@ -0,0 +1,14 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace RackDAT_API.Models;
+
+[Table("Carreras")]
+public class Carrera : BaseModel
+{
+    [PrimaryKey("id_Carreras")]
+    public int ID { get; set; }
+    [Column("carrera")]
+    public string carrera { get; set;}
+}
+
