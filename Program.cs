cc999d89f31c0c28582ce3e8862cadbf0ad00ff0
MrJ8585 <josea.solis@cetys.edu.mx>
2023-04-15 23:08:30 -0700

    conexion_supabase se hizo la conexion a supabase y se hicieron pruebas con las carreras

--- Program.cs
+++ Program.cs
@@ -1,25 +1,48 @@
+using RackDAT_API.Contracts;
+using RackDAT_API.Models;
+using Supabase;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddScoped<Supabase.Client>(_ =>
+    new Supabase.Client(
+        builder.Configuration["SupabaseUrl"],
+        builder.Configuration["SupabaseKey"],
+        new SupabaseOptions
+        {
+            AutoRefreshToken = true,
+            AutoConnectRealtime = true
+        }));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.MapPost("/carreras", async (
+    CreateCarrera request,
+    Supabase.Client client) =>
+{
+    var carrera = new Carrera
+    {
+        carrera = request.carrera
+    };
+
+    var response = await client.From<Carrera>().Insert(carrera);
+
+    var newCarrera = response.Models.First();
+
+    return Results.Ok("Se ha agregado correctamente la carrera: " + carrera.carrera + " con ID: " + carrera.ID);
+
+});
+
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
