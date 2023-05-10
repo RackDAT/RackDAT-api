0cca8bfcddd4cf0276eff2097ee87ff1eba51409
MrJ8585 <josea.solis@cetys.edu.mx>
2023-05-09 21:44:32 -0700

    se me olvido agregar este archivo XD

---
+++ Contracts/CorreoRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RackDAT_API.Contracts
+{
+    public class CorreoRequest
+    {
+        [Required]
+        public string correo { get; set; }
+    }
+}
